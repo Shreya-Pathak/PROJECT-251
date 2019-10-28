e24b7f38b84431436b0e2f2cf7daadf9b045e7d6
ali6947 <50691613+ali6947@users.noreply.github.com>
2019-10-28 14:14:28 +0530

    Add files via upload
    just user_name needs to be added now

--- qbank/views.py
+++ qbank/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UploadFileForm
+from .functions import handle_uploaded_file
 from utils.iniparser import iniparser
-
+import os
 from .models import Qbank_Main, Qbank_sub, Users
 
 def index(request):
@@ -12,15 +13,28 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            request.session['main_list'] = iniparser(request.FILES['file'])
-            print(request.session['main_list'])
+            #handle_uploaded_file(request.FILES['file'])
+            f = request.FILES['file']
+            contents = f.read()
+            with open(f.name,'wb+') as upf:
+                upf.write(contents)
+            #print(contents)
+            print(iniparser(f.name))
+            request.session['main_list'] = iniparser(f.name)
+            os.remove(f.name)
+            #print(request.session['main_list'])
             return HttpResponseRedirect('/qbank/a')
+            #return HttpResponse(contents)
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
 
 def add(request):
     main_list = request.session['main_list']
+    qbno_data=Qbank_Main.objects.values_list('QbankNo', flat=True)
+    if len(qbno_data)==0:
+        qbno_data=[0]
+    print(max(qbno_data)+1)
     #main_list = [{'user': 'dummy', 'qbankno': 1, 'content': 'first question feels', 'difficulty': 'Easy', 'tags': 'some,here,jjjj,kioo', 'chapter': 'buga', 'section': 'nuga', 'answer': None, 'part_1': 'first', 'part_2': 'jkkkkk', 'isModule': True, 'number_of_parts': 2, 'answer_1': 'lleell', 'answer_2': None, 'marks_1': 45, 'marks_2': 21, 'marks': 0}, {'user': 'dummy', 'qbankno': 2, 'content': 'first question feels', 'difficulty': 'easy', 'tags': 'some,here,jjjj,kioo', 'chapter': 'buga', 'section': 'nuga', 'answer': 'jiyooooo', 'part_1': 'first', 'part_2': 'jkkkkk', 'isModule': True, 'number_of_parts': 2, 'answer_1': 'loooooo', 'answer_2': None, 'marks_1': 45, 'marks_2': 66, 'marks': 0}]
     for x in main_list:
         print(x)
@@ -35,7 +49,7 @@ def add(request):
         q.Section = x['section']
         q.IsModule = x['isModule']
         q.Owner = x['user']
-        q.QbankNo = x['qbankno']
+        q.QbankNo = max(qbno_data)+1
         q.save()
         if(ism):
         	for i in range(x['number_of_parts']):
