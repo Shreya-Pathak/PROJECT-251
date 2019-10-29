6672ed4965eab3b73759689649f7001e297e3903
TarushGoyal <43519385+TarushGoyal@users.noreply.github.com>
2019-10-29 14:23:37 +0530

    added accounts, templates and sent_emails

---
+++ accounts/views.py
@@ -0,0 +1,75 @@
+#from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.urls import reverse_lazy
+from django.views import generic
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.auth.models import User
+
+class MyUser(User):
+    def __init__(self, *args, **kwargs):
+        is_prof = True
+        super(MyUser, self).__init__(*args, **kwargs)
+	
+
+class UserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    password1 = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
+
+    class Meta:
+        model = MyUser
+        fields = ("username",)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+        
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
+    
+# from .forms import NameForm
+
+# def my_login(request):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # create a form instance and populate it with data from the request:
+#         form = NameForm(request.POST)
+#         # check whether it's valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required
+#             # ...
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/thanks/')
+
+#     # if a GET (or any other method) we'll create a blank form
+#     else:
+#         form = NameForm()
+
+#     return render(request, 'name.html', {'form': form})
+
+
