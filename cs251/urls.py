b5772c0359fe65e8cd6f141c7ae570613ace0192
TarushGoyal <43519385+TarushGoyal@users.noreply.github.com>
2019-11-25 13:48:42 +0530

    updated urls

--- cs251/urls.py
+++ cs251/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from django.conf.urls import url
 from core import views as core_views
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 """
 urlpatterns = [
 	#path('', include('core.urls')),\
@@ -32,6 +33,7 @@ urlpatterns = [
 urlpatterns = [
 	path('admin/', admin.site.urls),
     # url(r'^home$', core_views.home, name='home'),
+    url(r'^$', RedirectView.as_view(url='/login/')),
     url(r'^login/$',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     url(r'^logout/$',auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     url(r'^signup/$', core_views.signup, name='signup'),
